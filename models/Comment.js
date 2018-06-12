7e1961485ce897f9e98838f3e884bbe93c0229b4
mkarczewski <matkal13>
2018-06-12 09:52:08 +0200

    Mongoose model updated

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+var mongoose = require("mongoose");
+
+var CommentSchema = new mongoose.Schema({
+    body: String,
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    article: { type: mongoose.Schema.Types.ObjectId, ref: 'Article' },
+}, {timestamps: true});
+
+CommentSchema.methods.toJSONFor = function(user) {
+    return {
+        id: this._id,
+        body: this.body,
+        createdAt: this.createdAt,
+        author: this.author.toProfileJSONFor(user)
+    }
+};
--- models/User.js
+++ models/User.js
@@ -9,6 +9,8 @@ var UserSchema = new mongoose.Schema({
     email: { type: String, lowercase: true, required: [true, "Can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true },
     bio: String,
     image: String,
+    favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Article' }],
+    following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     hash: String,
     salt: String
 }, { timestamps: true });
@@ -45,6 +47,51 @@ UserSchema.methods.toAuthJSON = function () {
         bio: this.bio,
         image: this.image
     }
+};
+
+UserSchema.methods.toFrofileJSONFor = function (user) {
+    return {
+        username: this.username,
+        bio: this.bio,
+        image: this.image || 'https://d2htdayykptdg9.cloudfront.net/sites/550f21d98e6927763a0003dd/content_entry582c570a882534008b009285/5915b878f16b6a008b2da574/files/noname-avatar.jpg',
+        following: user ? user.isFollowing(this._id) : false
+    }
+};
+
+UserSchema.methods.favorite = function (id) {
+    if (this.favorites.indexOf(id) === -1) {
+        this.favorites.push(id);
+    }
+
+    return this.save();
+};
+
+UserSchema.methods.unfavorite = function (id) {
+    this.favorites.remove(id);
+    return this.save();
+};
+
+UserSchema.methods.isFavorite = function (id) {
+    return this.favorites.some(function (favoriteId) {
+        return favoriteId.toString() === id.toString();
+    });
+};
+
+UserSchema.methods.follow = function (id) {
+    if (this.following.indexOf(id) === -1) {
+        this.following.push(id);
+    }
+};
+
+UserSchema.methods.unfollow = function (id) {
+    this.following.remove(id);
+    return this.save();
+};
+
+UserSchema.methods.isFollowing = function (id) {
+    return this.following.some(function (followId) {
+        return followId.toString() === id.toString();
+    });
 };
 
 mongoose.model('User', UserSchema);
